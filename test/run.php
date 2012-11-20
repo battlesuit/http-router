18e61becdf79b6ff699b2a15343227cedf1e64fc
Thomas Monzel <tom.monzel@gmail.com>
2012-11-20 23:29:49 +0100

    autoloading via loader in test/run.php

--- test/run.php
+++ test/run.php
@@ -5,10 +5,7 @@ namespace test_bench {
   
   require_once 'bench.php';
   
-  # register default autoload functionality
-  spl_autoload_register(function($class) {
-    return spl_autoload(preg_replace('/(\p{Ll})(\p{Lu})/', '$1_$2', $class), '.php');
-  });
+  autoload_in('http', __DIR__."/http");
   
   $bench = new PackageTestBench();
   $bench->run_and_present_as_text();
