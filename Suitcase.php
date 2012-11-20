8679ef68a965bc92d4b164b4580fe9fa60e2ffb7
Thomas Monzel <tom.monzel@gmail.com>
2012-11-20 21:36:10 +0100

    Moved autoloading to new Suitcase.php

--- Suitcase.php
+++ Suitcase.php
@@ -1,5 +1,6 @@
 <?php
 namespace suitcase {
+  autoload_in('http', __DIR__."/lib/http");
   import('http', 'inflector');
 }
 ?>
--- lib/http/router.php
+++ lib/http/router.php
@@ -3,13 +3,6 @@ namespace http;
 use http\route\Collection as RouteCollection;
 use http\route\Scope as RouteScope;
 
-spl_autoload_register(function($class) {
-  $underscored_class = preg_replace('/(\p{Ll})(\p{Lu})/', '$1_$2', $class);
-  $file = __DIR__."/../".str_replace('\\', '/', strtolower($underscored_class)).".php";
-  
-  if(file_exists($file)) require $file;
-});
-
 /**
  * Base routing application
  *
